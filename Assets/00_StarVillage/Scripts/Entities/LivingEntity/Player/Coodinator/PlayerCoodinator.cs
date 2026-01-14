08a2d19d26d85c0b0bb9c62d78977c1b851c126a
liang2papa <somedays33@naver.com>
2026-01-14 15:16:00 +0900

    #4 폴더구조 업데이트

--- Assets/00_StarVillage/Scripts/Entities/LivingEntity/Player/Coodinator/PlayerCoodinator.cs
+++ Assets/00_StarVillage/Scripts/Entities/LivingEntity/Player/Coodinator/PlayerCoodinator.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// 플레이어 객체의 행동 조율
+/// </summary>
 public class PlayerCoodinator : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
