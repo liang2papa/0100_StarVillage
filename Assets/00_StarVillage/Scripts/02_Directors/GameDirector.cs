acdac2e0064ec8bf681cfac287299fc9d77111b2
liang2papa <somedays33@naver.com>
2026-01-14 15:13:40 +0900

    Update GameDirector.cs

--- Assets/00_StarVillage/Scripts/02_Directors/GameDirector.cs
+++ Assets/00_StarVillage/Scripts/02_Directors/GameDirector.cs
@@ -2,6 +2,8 @@ using System;
 using UnityEngine;
 
 // [Lv 2] 씬의 감독 (추상 클래스)
+// 게임 상태를 가지고 있으며 이를 알리는 역할을 함
+// 필드 관리자 또는 마을 관리자가 상속받아 사용
 public abstract class GameDirector : MonoBehaviour
 {
     public EGameState CurrentState { get; private set; }
